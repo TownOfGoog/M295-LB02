943ed879b080a62f92b71ad2383977ffcd706fc2
TownOfGoog <95412264+TownOfGoog@users.noreply.github.com>
2023-05-05 13:53:27 +0000

    Aufgaben 14 und 10 erfüllt

--- LB02.js
+++ LB02.js
@@ -1,308 +1,253 @@
-//Vorbereitungen und essentielles
+// Vorbereitungen und essentielles
 
-import express from "express";
-import session from "express-session"
+import express from 'express'
+import session from 'express-session'
 
-
-
-const app = express();
+const app = express()
 app.use(express.json())
-const port = 3000;
-let theID = "3"
-let temp;
+const port = 3000
+let theID = '3'
+let temp
 
-function newID(){
-    temp=parseInt(theID)
-    temp=temp+1
-    theID=toString(temp)
+function newID () {
+  temp = parseInt(theID)
+  temp = temp + 1
+  theID = toString(temp)
 }
-
 
 app.use(session({
-    secret: 'supersecret',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {}
+  secret: 'supersecret',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {}
 }))
-//Tasks erstellt mit ChatGPT
+// Tasks erstellt mit ChatGPT
 let tasks = [
-    {
-        "id": "1",
-        "titel": "Einkauf erledigen",
-        "ersteller": "Max Muster",
-        "email": "Max@Mustermail",
-        "erstelldatum": "2023-05-10",
-        "beendet": null
-    },
-    {
-        "id": "2",
-        "titel": "Wäsche waschen",
-        "ersteller": "Max Muster",
-        "email": "Max@Mustermail",
-        "erstelldatum": "2023-05-11",
-        "beendet": null
-    },
-    {
-        "id": "3",
-        "titel": "Meeting vorbereiten",
-        "ersteller": "Max Muster",
-        "email": "Max@Mustermail",
-        "erstelldatum": "2023-05-12",
-        "beendet": null
-    }
+  {
+    id: '1',
+    titel: 'Einkauf erledigen',
+    ersteller: 'Max Muster',
+    email: 'Max@Mustermail',
+    erstelldatum: '2023-05-10',
+    beendet: null
+  },
+  {
+    id: '2',
+    titel: 'Wäsche waschen',
+    ersteller: 'Max Muster',
+    email: 'Max@Mustermail',
+    erstelldatum: '2023-05-11',
+    beendet: null
+  },
+  {
+    id: '3',
+    titel: 'Meeting vorbereiten',
+    ersteller: 'Max Muster',
+    email: 'Max@Mustermail',
+    erstelldatum: '2023-05-12',
+    beendet: null
+  }
 ]
 
-//Quelle: Eigene Dokumente des modules 295
+// Quelle: Eigene Dokumente des modules 295
 
+// Hauptanforderungen
+// Hauptanforderungen
+// Hauptanforderungen
 
-
-
-//Hauptanforderungen
-//Hauptanforderungen
-//Hauptanforderungen
-
-//Hauptanforderung 1 GET /tasks
-//Quelle: Eigene Dokumente des modules 295
-function findALL() {
-    return tasks;
+// Hauptanforderung 1 GET /tasks
+// Quelle: Eigene Dokumente des modules 295
+function findALL () {
+  return tasks
 }
 
-app.get("/tasks", (req, res) => {
-    if(req.session.email==true){
-        res.json(findALL())
-        res.status()
-    }else{
-        return res.status(401).json({ "message": "Not logged in" })
-    }
+app.get('/tasks', (req, res) => {
+  if (req.session.email === true) {
+    res.json(findALL())
+    res.status()
+  } else {
+    return res.status(401).json({ message: 'Not logged in' })
+  }
 })
 
-
-
-//Hauptanforderung 2
-//Quelle: Eigene Dokumente des modules 295
-export function addNewTask(task) {
-    tasks = [...tasks, task];
+// Hauptanforderung 2
+// Quelle: Eigene Dokumente des modules 295
+export function addNewTask (task) {
+  tasks = [...tasks, task]
 }
 
-app.post("/tasks", (req, res) => {
-    if(req.session.email){
+app.post('/tasks', (req, res) => {
+  if (req.session.email) {
+    const now = new Date()
+    const nowJSON = now.toJSON()
 
-        let now = new Date();
-        let nowJSON = now.toJSON();
-
-        let newTask = 
-        {"id": newID(),
-        "titel": req.body.title,
-        "ersteller": "Max Muster",
-        "email": "Max@Mustermail",
-        "erstelldatum": nowJSON,
-        "beendet": null
-                }
-        //addNewTask(newTask)
-
-        res.status(201).json({
-            newTask
-        });
-    }else{
-        return res.status(401).json({ "message": "Not logged in" })
-    }
-})
-
-
-
-
-
-
-//Hauptanforderung 3
-//Quelle: Eigene Dokumente des modules 295
-function FINDING(id) {
-    let task = tasks.find(task => task.id === id)
-    return task
-}
-
-app.get("/tasks/:id", (req, res) => {
-    console.log(req.session.email)  
-    if(req.session.email){
-
-        let taskId = req.params.id;
-        let task = FINDING(taskId);
-
-
-        if (task) {
-            res.json(task);
-            res.status(200).json({
-                "message": 'Task found.'
-            })
-        } else {
-            res.status(404).json({
-                "message": 'Task NOT found.'
-            })
+    const newTask =
+        {
+          id: newID(),
+          titel: req.body.title,
+          ersteller: 'Max Muster',
+          email: 'Max@Mustermail',
+          erstelldatum: nowJSON,
+          beendet: null
         }
-    }else{
-        return res.status(401).json({ "message": "Not logged in" })
-    }
+        // addNewTask(newTask)
+
+    res.status(201).json({
+      newTask
+    })
+  } else {
+    return res.status(401).json({ message: 'Not logged in' })
+  }
 })
 
-
-
-
-//Hauptanforderung 4
-//Quelle: Eigene Dokumente des modules 295 und Beispiel von Robin Bühler
-function changeTask(task) {
-    tasks = tasks.map((t) => t.id == task.id ? task : t);
+// Hauptanforderung 3
+// Quelle: Eigene Dokumente des modules 295
+function FINDING (id) {
+  const task = tasks.find(task => task.id === id)
+  return task
 }
 
-app.put("/tasks/:id", (req, res) => {
-    if(req.session.email){
+app.get('/tasks/:id', (req, res) => {
+  console.log(req.session.email)
+  if (req.session.email) {
+    const taskId = req.params.id
+    const task = FINDING(taskId)
 
-        let now = new Date();
-        let nowJSON = now.toJSON();
-
-
-        let id = req.params.id
-        let NewTask = {
-            "id": id,
-            "titel": req.body.title,
-            "ersteller": "Max Muster",
-            "email": "Max@Mustermail",
-            "erstelldatum": nowJSON,
-            "beendet": null
-        }
-
-
-        if (id) {
-            changeTask(NewTask);
-            res.status(200).json({
-                "message": 'Task NOT found.'
-            })
-        } else {
-            res.status(404).json({
-                "message": 'Task NOT found.'
-            })
-        }
-
-        res.json(FINDING(id))
-    }else{
-        return res.status(401).json({ "message": "Not logged in" })
+    if (task) {
+      res.json(task)
+      res.status(200).json({
+        message: 'Task found.'
+      })
+    } else {
+      res.status(404).json({
+        message: 'Task NOT found.'
+      })
     }
+  } else {
+    return res.status(401).json({ message: 'Not logged in' })
+  }
 })
 
-
-
-//Hauptanforderung 5
-//Quelle: Eigene Dokumente des modules 295 und Beispiel von Robin Bühler
-function removeTask(id) {
-    tasks = tasks.filter((t) => t.id !== id);
+// Hauptanforderung 4
+// Quelle: Eigene Dokumente des modules 295 und Beispiel von Robin Bühler
+function changeTask (task) {
+  tasks = tasks.map((t) => t.id === task.id ? task : t)
 }
 
-app.delete("/tasks/:id", (req, res) => {
-    if(req.session.email){
+app.put('/tasks/:id', (req, res) => {
+  if (req.session.email) {
+    const now = new Date()
+    const nowJSON = now.toJSON()
 
-    
-        let id = req.params.id
-        if (id) {
-            res.json(FINDING(id))
-            res.status(202).json({
-                "message": 'Task  found.'
-            })
-            res.json(removeTask(id))
-        } else {
-            res.status(404).json({
-                "message": 'Task NOT found.'
-            })
-        }
-    }else{
-        return res.status(401).json({ "message": "Not logged in" })
+    const id = req.params.id
+    const NewTask = {
+      id,
+      titel: req.body.title,
+      ersteller: 'Max Muster',
+      email: 'Max@Mustermail',
+      erstelldatum: nowJSON,
+      beendet: null
     }
 
+    if (id) {
+      changeTask(NewTask)
+      res.status(200).json({
+        message: 'Task NOT found.'
+      })
+    } else {
+      res.status(404).json({
+        message: 'Task NOT found.'
+      })
+    }
+
+    res.json(FINDING(id))
+  } else {
+    return res.status(401).json({ message: 'Not logged in' })
+  }
 })
 
+// Hauptanforderung 5
+// Quelle: Eigene Dokumente des modules 295 und Beispiel von Robin Bühler
+function removeTask (id) {
+  tasks = tasks.filter((t) => t.id !== id)
+}
 
+app.delete('/tasks/:id', (req, res) => {
+  if (req.session.email) {
+    const id = req.params.id
+    if (id) {
+      res.json(FINDING(id))
+      res.status(202).json({
+        message: 'Task  found.'
+      })
+      res.json(removeTask(id))
+    } else {
+      res.status(404).json({
+        message: 'Task NOT found.'
+      })
+    }
+  } else {
+    return res.status(401).json({ message: 'Not logged in' })
+  }
+})
 
+// Authentifizierung
+// Authentifizierung
+// Authentifizierung
 
+// Sämtliche Authentifizierungs Sachen sind inspiriert von den SlyDev Slides und eigenarbeit des Modules 295
+// https://openscript.github.io/course-zli-m295/#/83?clicks=0
+// Vorarbeiten
+const password = 'm295'
 
-
-
-
-
-
-
-
-
-
-
-//Authentifizierung
-//Authentifizierung
-//Authentifizierung
-
-//Sämtliche Authentifizierungs Sachen sind inspiriert von den SlyDev Slides und eigenarbeit des Modules 295
-//https://openscript.github.io/course-zli-m295/#/83?clicks=0
-//Vorarbeiten
-const password = "m295"
-
-
-
-
-
-
-
-//Anforderung 6
+// Anforderung 6
 app.post('/login', function (req, res) {
-    let email = req.body.email;
-    let thisPassword = req.body.password;
+  const email = req.body.email
+  const thisPassword = req.body.password
 
-    if(!email || !thisPassword) {
-        return res.status(400).json({
-            error: "Passwort, oder Email Fehlen."
-        })
-    }
+  if (!email || !thisPassword) {
+    return res.status(400).json({
+      error: 'Passwort, oder Email Fehlen.'
+    })
+  }
 
-    if (thisPassword == password) {
-        req.session.email = true
-        return res.status(200).json({ email: req.session.email })
-    }
+  if (thisPassword === password) {
+    req.session.email = true
+    return res.status(200).json({ email: req.session.email })
+  }
 
-    return res.status(401).json({ "message": "Falsche Email, oder Passwort" })
-});
+  return res.status(401).json({ message: 'Falsche Email, oder Passwort' })
+})
 
-
-//Anforderung 7
+// Anforderung 7
 app.get('/verify', function (req, res) {
-    console.log(req.session)
-    if (req.session.email) {
-        return res.status(200).json({ email: req.session.email })
-    }
+  console.log(req.session)
+  if (req.session.email) {
+    return res.status(200).json({ email: req.session.email })
+  }
 
-    return res.status(401).json({ error: "Not logged in" })
+  return res.status(401).json({ error: 'Not logged in' })
 })
 
-
-
-//Anforderung 8
+// Anforderung 8
 app.delete('/logout', function (req, res) {
-
-	if (req.session.email) {
-		req.session.email = null
-		return res.status(204).json({"message": "Erfolgreich ausgelogt"})
-	}
-  return res.status(401).json({ "message": "Not logged in" })
+  if (req.session.email) {
+    req.session.email = null
+    return res.status(204).json({ message: 'Erfolgreich ausgelogt' })
+  }
+  return res.status(401).json({ message: 'Not logged in' })
 })
 
+// Zusätzliche Anforderungen
 
-
-
-
-
-
-
-
-//Zusätzliche Anforderungen
-
-
-app.get("/", (req, res) => {
-    res.status(406).json({"message":"Hier gibt es nichts"})
+app.get('/', (req, res) => {
+  res.status(406).json({ message: 'Hier gibt es nichts' })
 })
 
 app.listen(port, () => {
-    console.log('Server is running on port 3000');
-});
+  console.log('Server is running on port 3000')
+})
+
+app.get('/*', (req, res) => {
+  res.status(404).json({ message: 'Seite existiert nicht' })
+})
